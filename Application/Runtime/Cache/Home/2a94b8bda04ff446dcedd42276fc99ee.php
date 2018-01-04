f21bf47e8c122d9180ffd4d9dc99c339776bba20
15930167221 <1366860941@qq.com>
2018-01-04 16:14:51 +0800

    yxy

--- Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -1,1 +1,9 @@
-<?php if (!defined('THINK_PATH')) exit();?>asdf
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+前台页面
+</body>
+</html>
