17e04d312550ec3b95329e37a5d6d65ec9905d75
chengwenxue <2428109754@qq.com>
2018-01-04 15:58:45 +0800

    合并

--- Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -1,47 +1,13 @@
 <?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
 <html lang="en">
 <head>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<meta charset="utf-8">
-<meta name="renderer" content="webkit|ie-comp|ie-stand">
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no" />
-<meta http-equiv="Cache-Control" content="no-siteapp" />
-<!-- <link rel="Bookmark" href="favicon.ico" >
-<link rel="Shortcut Icon" href="favicon.ico" /> -->
-<!--[if lt IE 9]>
-<script type="text/javascript" src="/-/Public/lib/html5.js"></script>
-<script type="text/javascript" src="/-/Public/lib/respond.min.js"></script>
-<![endif]-->
-<link rel="stylesheet" type="text/css" href="/-/Public/static/h-ui/css/H-ui.min.css" />
-<link rel="stylesheet" type="text/css" href="/-/Public/static/h-ui.admin/css/H-ui.admin.css" />
-<link rel="stylesheet" type="text/css" href="/-/Public/lib/Hui-iconfont/1.0.8/iconfont.css" />
-<link rel="stylesheet" type="text/css" href="/-/Public/static/h-ui.admin/skin/default/skin.css" id="skin" />
-<link rel="stylesheet" type="text/css" href="/-/Public/static/h-ui.admin/css/style.css" />
-<!--[if IE 6]>
-<script type="text/javascript" src="http:///-/Public/lib.h-ui.net/DD_belatedPNG_0.0.8a-min.js" ></script>
-<script>DD_belatedPNG.fix('*');</script>
-<![endif]-->
-<!--/meta 作为公共模版分离出去-->
-
-<title>H-ui.admin v3.0</title>
-<meta name="keywords" content="H-ui.admin v3.0,H-ui网站后台模版,后台模版下载,后台管理系统模版,HTML后台模版下载">
-<meta name="description" content="H-ui.admin v3.0，是一款由国人开发的轻量级扁平化网站后台模板，完全免费开源的网站后台管理系统模版，适合中小型CMS后台系统。">
-=======
-=======
->>>>>>> a2d612aebf75f27d9a7b942920add72b153c0ebd
     <meta charset="UTF-8">
     <meta name="viewport" content="user-scalable=no">
     <title>快点—智慧餐厅</title>
-    <link rel="icon" href="/kuaidian/Public/Home/kdyd/images/logo1.png">
-    <link rel="stylesheet" href="/kuaidian/Public/Home/kdyd/css/index.css">
-    <link rel="stylesheet" href="/kuaidian/Public/Home/kdyd/css/base.css">
-    <link rel="stylesheet" href="/kuaidian/Public/Home/kdyd/css/base2.css">
-<<<<<<< HEAD
->>>>>>> a2d612aebf75f27d9a7b942920add72b153c0ebd
-=======
->>>>>>> a2d612aebf75f27d9a7b942920add72b153c0ebd
+    <link rel="icon" href="/-/Public/Home/kdyd/images/logo1.png">
+    <link rel="stylesheet" href="/-/Public/Home/kdyd/css/index.css">
+    <link rel="stylesheet" href="/-/Public/Home/kdyd/css/base.css">
+    <link rel="stylesheet" href="/-/Public/Home/kdyd/css/base2.css">
 </head>
 <body>
     <!--头部-->
@@ -50,7 +16,7 @@
             <div class="city">
                 <div class="ee">
                     <span>北京</span>
-                    <img src="/kuaidian/Public/Home/kdyd/images/jiantou2.png" alt="">
+                    <img src="/-/Public/Home/kdyd/images/jiantou2.png" alt="">
                 </div>
             </div>
 
@@ -58,7 +24,7 @@
             <div class="lookup">
                 <div class="rr">
                     <a href="#">
-                        <img src="/kuaidian/Public/Home/kdyd/images/loogup.png" alt="">
+                        <img src="/-/Public/Home/kdyd/images/loogup.png" alt="">
                         <div class="zz">
                             <input type="text" class="search-txt" placeholder="搜索商家名称"/>
                         </div>
@@ -67,31 +33,16 @@
             </div>
             <div class="person">
                 <div class="ww">
-                    <img src="/kuaidian/Public/Home/kdyd/images/person.JPG" alt="">
+                    <img src="/-/Public/Home/kdyd/images/person.JPG" alt="">
                 </div>
             </div>
         </div>
     </header>
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<!--_footer 作为公共模版分离出去-->
-<script type="text/javascript" src="/-/Public/lib/jquery/1.9.1/jquery.min.js"></script> 
-<script type="text/javascript" src="/-/Public/lib/layer/2.4/layer.js"></script>
-<script type="text/javascript" src="/-/Public/static/h-ui/js/H-ui.js"></script> 
-<script type="text/javascript" src="/-/Public/static/h-ui.admin/js/H-ui.admin.page.js"></script> 
-<!--/_footer /作为公共模版分离出去-->
-=======
-=======
->>>>>>> a2d612aebf75f27d9a7b942920add72b153c0ebd
     <!--中间广告-->
     <section class="ad_pic">
-        <img src="/kuaidian/Public/Home/kdyd/images/shangpintu.png" alt="">
+        <img src="/-/Public/Home/kdyd/images/shangpintu.png" alt="">
     </section>
-<<<<<<< HEAD
->>>>>>> a2d612aebf75f27d9a7b942920add72b153c0ebd
-=======
->>>>>>> a2d612aebf75f27d9a7b942920add72b153c0ebd
 
     <!--中间分类-->
     <section class="job-module"> 
@@ -145,7 +96,7 @@
         </dd>
     </dl>
 </section>
-<script type="text/javascript" src="/kuaidian/Public/jquery/jquery.js"></script> 
+<script type="text/javascript" src="/-/Public/jquery/jquery.js"></script> 
 <script type="text/javascript">
   $(function(){ 
     $('.retrie dt a').click(function(){
@@ -168,13 +119,13 @@
   });
 </script> 
     <!--中间商品部分-->
-    
+    <?php if(is_array($data)): foreach($data as $key=>$): endforeach; endif; ?>
     <div class="zhongjian">
         <div class="shangpin">
             <div class="hezi">
                <div class="he">
                    <div class="tp">
-                       <img src="/kuaidian/Public/Home/kdyd/images/tupian.png" alt="">
+                       <img src="/-/Public/Home/kdyd/images/tupian.png" alt="">
                    </div>
                    <div class="seller">
                        <div class="dianming">
@@ -183,15 +134,15 @@
                        </div>
 
                        <div class="evaluate">
-                           <a href="#" class="start"> <img src="/kuaidian/Public/Home/kdyd/images/start.png" alt=""> </a>
+                           <a href="#" class="start"> <img src="/-/Public/Home/kdyd/images/start.png" alt=""> </a>
                            <span class="consume">￥20/人</span>
-                           <a href="#" class="quan"> <img src="/kuaidian/Public/Home/kdyd/images/quan.png" alt=""> </a>
-                           <a href="#" class="dian"> <img src="/kuaidian/Public/Home/kdyd/images/dian.png" alt=""> </a>
-                           <a href="#" class="ding"> <img src="/kuaidian/Public/Home/kdyd/images/ding.png" alt=""> </a>
+                           <a href="#" class="quan"> <img src="/-/Public/Home/kdyd/images/quan.png" alt=""> </a>
+                           <a href="#" class="dian"> <img src="/-/Public/Home/kdyd/images/dian.png" alt=""> </a>
+                           <a href="#" class="ding"> <img src="/-/Public/Home/kdyd/images/ding.png" alt=""> </a>
                        </div>
 
                        <div class="activity">
-                           <img src="/kuaidian/Public/Home/kdyd/images/tui.png" alt="">
+                           <img src="/-/Public/Home/kdyd/images/tui.png" alt="">
                            <span class="shang">商家已发起“微众代言”可获得 <span class="red">5.0%</span> </span>
                        </div>
                    </div>
@@ -203,12 +154,12 @@
     <!--底部-->
     <footer>
         <div class="foot">
-            <img src="/kuaidian/Public/Home/kdyd/images/shangjia2.png" alt="">
+            <img src="/-/Public/Home/kdyd/images/shangjia2.png" alt="">
             <a href="#"> <p>商家</p> </a>
         </div>
 
         <div class="food">
-            <img src="/kuaidian/Public/Home/kdyd/images/geren2.png" alt="">
+            <img src="/-/Public/Home/kdyd/images/geren2.png" alt="">
             <a href="#"> <p>我的</p> </a>
         </div>
     </footer>
